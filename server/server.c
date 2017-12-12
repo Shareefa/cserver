0185cf1d115027b42a09e33f4b7c15db6a00c890
shareefa <abdullah.shareef@gmail.com>
2017-12-11 22:54:08 -0500

    client

--- server/server.c
+++ server/server.c
@@ -13,6 +13,77 @@ void error(char *msg)
 	exit(1);
 }
 
+void tokenize(char* line, struct movie* movie){
+    int front = 0;
+    int back = 0;
+
+    int charPointer = 0;
+    int valueIndex = 0;
+    movie->values[27] = NULL;
+
+    while(line[charPointer] != '\0'){
+        if(line[charPointer] == ' '){
+            charPointer++;
+            while(line[charPointer] == ' '){
+                charPointer++;
+            }
+        }
+        front = charPointer;
+        if(line[charPointer] == '"'){
+            charPointer++;
+            while(line[charPointer] != '"'){
+                charPointer++;
+            }
+        }
+        if(line[charPointer] == '"'){
+
+            back = charPointer;
+            movie->values[valueIndex] = (char*) realloc(movie->values[valueIndex], sizeof(char)*(back-front+2));
+        //  movie->values[valueIndex] = (char*) malloc (sizeof(char)*(back-front+2));//char arr[(back - front + 2)];
+            movie->values[valueIndex][back-front+1] = '\0';
+            strncpy(movie->values[valueIndex], &line[front], back-front+1);
+            charPointer++;
+            charPointer++;
+            valueIndex++;
+            continue;
+        }
+
+
+        if(line[front] == ','){
+            movie->values[valueIndex] = "";
+            valueIndex++;
+            front = 0;
+            charPointer++;
+            continue;
+        }
+
+        while(line[charPointer] != ',' && line[charPointer] != '\0'){
+            charPointer++;
+        }
+
+        back = charPointer-1;
+
+        while(line[back] == ' ' || line[back] == '\r' || line[back] == '\n'){
+            back--;
+        }
+        movie->values[valueIndex] = (char*) realloc(movie->values[valueIndex], sizeof(char)*(back-front+2));
+    //  movie -> values[valueIndex] = (char*) malloc (sizeof(char)*(back-front+2));
+        movie->values[valueIndex][back-front+1] = '\0';
+        strncpy(movie->values[valueIndex], &line[front], back-front+1);
+    //  printf("%s\n", movie->values[valueIndex]);
+        if(line[charPointer] != '\0'){
+            charPointer++;
+        }
+        valueIndex++;
+
+    }
+    
+    if(movie->values[27] == NULL){
+        movie->values[27] = "";
+    }
+
+}
+
 int main(int argc, char *argv[])
 {   
 
